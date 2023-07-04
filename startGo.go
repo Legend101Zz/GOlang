489ad9720e3f479ed273c351e5f931503950b8ff
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-04 20:58:20 +0530

    initial learning

--- startGo.go
+++ startGo.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+
+func add(x,y int)int{
+	return x + y
+}
 
 func main(){
+const name ="Mrigesh"
 
-	fmt.Println("hel;lo")
+	fmt.Printf(" hello %s\n , my fav num is ,%d \n",name, rand.Intn(100));
+	fmt.Println(" hello ",name , "my fav num is , \n", rand.Intn(100),"also",math.Pi, "adding", add(10,100))
 }

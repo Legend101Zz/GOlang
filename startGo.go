3faee6651da6e53cefd4e19bbff24c24e9b879b3
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-09 09:49:51 +0530

    maps

--- startGo.go
+++ startGo.go
@@ -19,6 +19,14 @@ copy(cxs,xs)
 xs[0]= "new"
 
 
+mp := map[string]int {
+"mrigesh" :1,
+"nunu" : 2, 
+}
+
+for k,v := range mp {
+	fmt.Println(k,v)
+}
 	// fmt.Printf(" hello %s\n , my fav num is ,%d \n",name, rand.Intn(100));
 		fmt.Println(xs,"\n");
 		fmt.Println(cxs,"\n");

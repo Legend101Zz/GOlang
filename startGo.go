41a0d33b532a28f76ab68dbe8670ff6e41b75a85
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-09 19:06:31 +0530

    variatic param functions

--- startGo.go
+++ startGo.go
@@ -5,6 +5,23 @@ import (
 )
 
 
+func sum (unlimited ...int)int{
+
+	fmt.Println ("No. are :",unlimited)
+n := 0 
+	for _,v := range unlimited {
+n += v
+	}
+
+	return n
+}
+
+type person struct {
+
+	name string 
+	age int
+	home string}
+
 func add(x,y int)int{
 	return x + y
 }
@@ -20,17 +37,35 @@ xs[0]= "new"
 
 
 mp := map[string]int {
-"mrigesh" :1,
+"mrigesh" :0,
 "nunu" : 2, 
 }
 
 for k,v := range mp {
 	fmt.Println(k,v)
 }
-	// fmt.Printf(" hello %s\n , my fav num is ,%d \n",name, rand.Intn(100));
-		fmt.Println(xs,"\n");
-		fmt.Println(cxs,"\n");
-		
-	
 
+
+v,ok := mp["mrigesh"]
+if(ok){
+	fmt.Println(v,ok)
+}else{
+fmt.Println("key not vfound")
+}
+
+
+p1:= person {
+	name : "Mrigesh",
+	age : 20 ,
+	home : "Kullu",
+}
+// fmt.Printf(" hello %s\n , my fav num is ,%d \n",name, rand.Intn(100));
+		// fmt.Println(xs,"\n");
+		// fmt.Println(cxs,"\n");gi
+
+		fmt.Println(p1)
+		
+		x:= sum(1,2,3,3,1,3,33,3,6,7,8,100,111,2,2,1,1,1,1,3,3,34,5,6,8,9)
+	
+fmt.Println("sum is :-", x)
 }
